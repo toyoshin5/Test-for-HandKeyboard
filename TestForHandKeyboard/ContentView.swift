19edd2c0750f369832d048f1767562988bbd7755
Shingo TOYODA <toyodasinngo@gmail.com>
2023-06-08 19:05:59 +0900

    Initial Commit

---
+++ TestForHandKeyboard/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TestForHandKeyboard
+//
+//  Created by Shin Toyo on 2023/06/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestForHandKeyboard/TestForHandKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestForHandKeyboardApp.swift
+//  TestForHandKeyboard
+//
+//  Created by Shin Toyo on 2023/06/08.
+//
+
+import SwiftUI
+
+@main
+struct TestForHandKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
